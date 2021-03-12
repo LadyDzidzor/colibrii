aa46de52ca9e86c277757c776cb86966e57d8ba9
Lady Dzidzor <ladydzidzor@gmail.com>
2021-03-12 01:36:11 +0000

    created a faq page and changed footer, nav, and added services

--- js/webflow.js
+++ js/webflow.js
@@ -1,3 +1,51 @@
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ""
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+
+}
+
+navSlide();
+
+$(".open").click(function () {
+  var container = $(this).parents(".topic");
+  var answer = container.find(".answer");
+  var trigger = container.find(".faq-t");
+
+  answer.slideToggle(200);
+
+  if (trigger.hasClass("faq-o")) {
+    trigger.removeClass("faq-o");
+  }
+  else {
+    trigger.addClass("faq-o");
+  }
+
+  if (container.hasClass("expanded")) {
+    container.removeClass("expanded");
+  }
+  else {
+    container.addClass("expanded");
+  }
+});
+
+
 /*!
  * Webflow: Front-end site library
  * @license MIT
